810234752c38edc91bdf54c40b780dd00f84aeed
jngsh <tse8ce17@gmail.com>
2024-07-30 16:21:08 +0900

    feat: Add find itemType and count

---
+++ src/main/java/com/exam/controller/CategoriesController.java
@@ -0,0 +1,31 @@
+package com.exam.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.exam.dto.ItemCategoryDTO;
+import com.exam.service.CategoriesService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/home")
+public class CategoriesController {
+
+	CategoriesService categorieService;
+
+    public CategoriesController(CategoriesService categorieService) {
+        this.categorieService = categorieService;
+    }
+    
+    @GetMapping("/item-category")
+    public List<ItemCategoryDTO> findItemCategory() {
+    	List<ItemCategoryDTO> list = categorieService.findItemCategory();
+    	return list;
+    }
+    
+}
--- src/main/java/com/exam/service/CategoriesServiceImpl.java
+++ src/main/java/com/exam/service/CategoriesServiceImpl.java
@@ -1,5 +1,28 @@
 package com.exam.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.exam.config.CategoriesMapper;
+import com.exam.dto.ItemCategoryDTO;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
 public class CategoriesServiceImpl implements CategoriesService {
+	
+	CategoriesMapper categoriesMapper;
+	
+	public CategoriesServiceImpl(CategoriesMapper categoriesMapper) {
+		this.categoriesMapper = categoriesMapper;
+	}
+
+	@Override
+	public List<ItemCategoryDTO> findItemCategory() {
+		List<ItemCategoryDTO> result = categoriesMapper.findItemCategory();
+		return result;
+	}
 
 }
