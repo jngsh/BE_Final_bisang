ef0ffb254bb1b637802833e4f48aa6429a4b9083
dlduddms08 <dlduddms08@icloud.com>
2024-08-12 11:07:41 +0900

    feat: Add Find Pw By Email

---
+++ src/main/java/com/exam/dto/MailDTO.java
@@ -0,0 +1,27 @@
+package com.exam.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@ToString
+public class MailDTO {
+
+  String toAddresss;
+  String title;
+  String message;
+  String fromAddress;
+   
+}
