5df5206e6b47748b4925fc0ad3a97d007065d89e
dlduddms08 <dlduddms08@icloud.com>
2024-08-12 14:25:02 +0900

    fixing: Add .env

--- src/main/java/com/exam/Application.java
+++ src/main/java/com/exam/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -12,6 +13,9 @@ import lombok.extern.slf4j.Slf4j;
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
+		System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
 		SpringApplication.run(Application.class, args);
 	}
 
