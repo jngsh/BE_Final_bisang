524a3678cff7cf5ef37e71892f880d86d1fa1857
jngsh <tse8ce17@gmail.com>
2024-08-22 13:52:00 +0900

    refactor: Change find category to support i18n

--- src/main/java/com/exam/service/CategoriesService.java
+++ src/main/java/com/exam/service/CategoriesService.java
@@ -4,14 +4,10 @@ import java.util.List;
 import java.util.Map;
 
 import com.exam.dto.DiscountsDTO;
-import com.exam.dto.ItemCategoryDTO;
 
 public interface CategoriesService {
 
-	public List<ItemCategoryDTO> findItemCategory();
-	
-//	public List<Map<String, Object>> findCategories();
+	public List<Map<String, Object>> findItemCategory();
 	public List<Map<String, Object>> findCategoryByPetType(String petType);
-	
 	public List<DiscountsDTO> findProductsByCategory(Map<String, Object> type);
 }
