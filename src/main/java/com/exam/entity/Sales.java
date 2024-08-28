cb920ebc2e15bf720c783b51cbe7294a04fa7ddf
jngsh <tse8ce17@gmail.com>
2024-08-28 17:24:34 +0900

    fix: Fix table name

--- src/main/java/com/exam/entity/Sales.java
+++ src/main/java/com/exam/entity/Sales.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,6 +25,7 @@ import lombok.ToString;
 @Setter
 @ToString
 @Entity
+@Table(name = "sales")
 public class Sales {
 	
 	@Id
