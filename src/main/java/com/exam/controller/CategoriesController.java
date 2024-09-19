6f394897f44a7c4c568f0839c650bf2401dc3d1c
jngsh <tse8ce17@gmail.com>
2024-09-20 08:02:31 +0900

    Refactor: Update CategoriesController to use ResponseEntity

--- src/main/java/com/exam/controller/CategoriesController.java
+++ src/main/java/com/exam/controller/CategoriesController.java
@@ -1,9 +1,10 @@
 package com.exam.controller;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,32 +14,46 @@ import com.exam.service.CategoriesService;
 
 import lombok.extern.slf4j.Slf4j;
 
-
 @Slf4j
 @RestController
 public class CategoriesController {
 
-	CategoriesService categorieService;
+    CategoriesService categorieService;
 
     public CategoriesController(CategoriesService categorieService) {
         this.categorieService = categorieService;
     }
-    
+
     @GetMapping("/home/item-category")
-    public List<Map<String, Object>> findItemCategory() {
-    	List<Map<String, Object>> list = categorieService.findItemCategory();
-    	return list;
+    public ResponseEntity<List<Map<String, Object>>> findItemCategory() {
+        try {
+            List<Map<String, Object>> list = categorieService.findItemCategory();
+            return ResponseEntity.ok(list);
+        } catch (Exception e) {
+            log.error("Error in findItemCategory: ", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(List.of());
+        }
     }
-    
+
     @GetMapping("/category/type/{petType}")
-    public List<Map<String, Object>> findCategoryByPetType(@PathVariable("petType") String petType) {
-        return categorieService.findCategoryByPetType(petType);
+    public ResponseEntity<List<Map<String, Object>>> findCategoryByPetType(@PathVariable String petType) {
+        try {
+            List<Map<String, Object>> list = categorieService.findCategoryByPetType(petType);
+            return ResponseEntity.ok(list);
+        } catch (Exception e) {
+            log.error("Error in findCategoryByPetType: ", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(List.of());
+        }
     }
-    
+
     @GetMapping("/category/products-list")
-    public List<DiscountsDTO> findAllCategoryProducts() {
-        List<DiscountsDTO> list = categorieService.findAllCategoryProducts();
-        return list;
+    public ResponseEntity<List<DiscountsDTO>> findAllCategoryProducts() {
+        try {
+            List<DiscountsDTO> list = categorieService.findAllCategoryProducts();
+            return ResponseEntity.ok(list);
+        } catch (Exception e) {
+            log.error("Error in findAllCategoryProducts: ", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(List.of());
+        }
     }
-    
 }
